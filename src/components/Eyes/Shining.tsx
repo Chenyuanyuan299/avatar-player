76c2db00f0c0b2fa8c88d3e316e104ac0eeec5c7
airfri <lq_niki@163.com>
2021-11-19 10:17:49 +0800

    eye_hair_mouth

---
+++ src/components/Eyes/Shining.tsx
@@ -0,0 +1,87 @@
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup() {
+        return () => (
+            <svg
+                style={{
+                width: '100%',
+                height: '12%',
+                position: 'absolute',
+                top: '7%',
+                }}
+                width="96"
+                height="48"
+                viewBox="0 0 96 48"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                 >
+                <ellipse
+                cx="16.1171"
+                cy="28.9268"
+                rx="13"
+                ry="14"
+                transform="rotate(-6.77646 16.1171 28.9268)"
+                fill="black"
+                />
+                <ellipse
+                cx="80.1486"
+                cy="18.9231"
+                rx="13"
+                ry="14"
+                transform="rotate(-6.27568 80.1486 18.9231)"
+                fill="black"
+                />
+                <ellipse
+                cx="14.1171"
+                cy="20.9268"
+                rx="4"
+                ry="4"
+                transform="rotate(-6.77646 16.1171 28.9268)"
+                fill="white"
+                />
+                <ellipse
+                cx="13.1171"
+                cy="31.9268"
+                rx="3"
+                ry="3"
+                transform="rotate(-6.77646 16.1171 28.9268)"
+                fill="white"
+                />
+                <ellipse
+                cx="21.9171"
+                cy="30.9268"
+                rx="3"
+                ry="3"
+                transform="rotate(-6.77646 16.1171 28.9268)"
+                fill="white"
+                />
+                <ellipse
+                cx="80.1486"
+                cy="18.9231"
+                rx="4"
+                ry="4"
+                transform="rotate(-6.77646 16.1171 28.9268)"
+                fill="white"
+                />
+                <ellipse
+                cx="78.1486"
+                cy="29.9231"
+                rx="3"
+                ry="3"
+                transform="rotate(-6.77646 16.1171 28.9268)"
+                fill="white"
+                />
+                <ellipse
+                cx="86.1486"
+                cy="28.9231"
+                rx="3"
+                ry="3"
+                transform="rotate(-6.77646 16.1171 28.9268)"
+                fill="white"
+                />
+
+            </svg>
+        );
+    },
+});
